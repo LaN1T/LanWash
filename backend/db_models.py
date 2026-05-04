5655537f2500ff0a362114c0e06fdb8b0bb0bbfb
lan1t <e-smigunov@yandex.ru>
2026-05-04 23:16:57 +0300

    добавить новый README

--- backend/db_models.py
+++ backend/db_models.py
@@ -56,6 +56,7 @@ class Appointment(Base):
     paidPrice = Column(Integer, nullable=False, default=0)
     isModifiedByAdmin = Column(Integer, nullable=False, default=0)
     isModifiedByWasher = Column(Integer, nullable=False, default=0)
+    isSeenByClient = Column(Integer, nullable=False, default=1)
     originalPrice = Column(Integer, nullable=False, default=0)
     assignedWasher = Column(String, nullable=False, default='[]')
     promoId = Column(String, ForeignKey('promos.id'), nullable=True)
