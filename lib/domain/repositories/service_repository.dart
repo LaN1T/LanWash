326adcaa94fc49a5555a2b8fb9a589dd4f7d68f4
lan1t <e-smigunov@yandex.ru>
2026-05-29 23:08:18 +0300

    рефакторинг(flutter): удалена мёртвая дублирующая архитектура (lib/app.dart, lib/data, lib/domain, lib/presentation)

--- lib/domain/repositories/service_repository.dart
+++
@@ -1,8 +0,0 @@
-import '../entities/service.dart';
-
-abstract class ServiceRepository {
-  Future<List<Service>> getServices();
-  Future<bool> createService(Service service);
-  Future<bool> updateService(Service service);
-  Future<bool> deleteService(String id);
-}
