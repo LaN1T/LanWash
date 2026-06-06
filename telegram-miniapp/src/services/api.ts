f251bb57548fe1f277022e3fcef832a63144bb8b
lan1t <e-smigunov@yandex.ru>
2026-06-06 20:50:02 +0300

    фича(мини-приложение): создать каркас React-проекта с hooks, сервисами и сторами

---
+++ telegram-miniapp/src/services/api.ts
@@ -0,0 +1,27 @@
+import axios from 'axios'
+
+export const api = axios.create({
+  baseURL: '/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('lanwash_token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem('lanwash_token')
+      window.location.reload()
+    }
+    return Promise.reject(error)
+  }
+)
