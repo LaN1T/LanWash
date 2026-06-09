2588c53e18ff67686a96a90359cccc240a21aa94
lan1t <e-smigunov@yandex.ru>
2026-06-09 17:12:27 +0300

    feat(stage1): landing SEO meta tags, Open Graph, JSON-LD, analytics init

--- landing/js/main.js
+++ landing/js/main.js
@@ -238,10 +238,41 @@ function initCarousel() {
 }
 
 function trackEvent(eventName, params) {
-  if (window.gtag) {
+  if (typeof gtag === 'function') {
     gtag('event', eventName, params || {});
   }
-  // Stub for future analytics integrations
+  if (typeof ym === 'function' && window.LANWASH_YM_ID) {
+    ym(window.LANWASH_YM_ID, 'reachGoal', eventName);
+  }
+}
+
+function initAnalytics() {
+  const gaId = window.LANWASH_GA_ID;
+  if (gaId) {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=' + gaId;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', gaId);
+  }
+
+  const ymId = window.LANWASH_YM_ID;
+  if (ymId) {
+    const script = document.createElement('script');
+    script.innerHTML =
+      '(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};' +
+      'm[i].l=1*new Date();' +
+      'for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}' +
+      'k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})' +
+      '(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");' +
+      'ym(' + ymId + ', "init", {clickmap:true,trackLinks:true,accurateTrackBounce:true});';
+    document.head.appendChild(script);
+  }
 }
 
 function initMobileMenu() {
@@ -284,6 +315,7 @@ function initModal() {
 }
 
 document.addEventListener('DOMContentLoaded', () => {
+  initAnalytics();
   initTheme();
   initLang();
   renderServices();
